4991f9b789d7e7e94c87eface3ee7beaf3bbbbb6
Jeremy <jeremylimjw@gmail.com>
2022-02-20 05:02:04 +0800

    clean up access rightsa

--- models/AccessRight.js
+++ models/AccessRight.js
@@ -1,7 +1,5 @@
 const { DataTypes } = require('sequelize');
-const ViewType = require('../common/ViewType');
 const { sequelize } = require('../db');
-const Log = require('./Log');
 
 const AccessRight = sequelize.define('access_right', {
     has_write_access: {
@@ -13,35 +11,4 @@ const AccessRight = sequelize.define('access_right', {
     createdAt: 'created_at', // Standardize 'createdAt' column name to 'created_at'
 });
 
-
-async function createAccessRight(access_right, from, to) {
-    // Delete the access right
-    await AccessRight.upsert(access_right);
-
-    // Find the view name using `view_id`
-    const viewKey = Object.keys(ViewType).filter(key => ViewType[key].id == access_right.view_id);
-
-    // Record in logs
-    await Log.create({ 
-        employee_id: from.id, 
-        view_id: ViewType.ADMIN.id,
-        text: `${from.name} granted ${ViewType[viewKey].name} access from ${to.name}`, 
-    });
-}
-
-async function deleteAccessRight(predicate, from, to) {
-    // Delete the access right
-    await AccessRight.destroy({ where: predicate});
-
-    // Find the view name using `view_id`
-    const viewKey = Object.keys(ViewType).filter(key => ViewType[key].id == predicate.view_id);
-
-    // Record in logs
-    await Log.create({ 
-        employee_id: from.id, 
-        view_id: ViewType.ADMIN.id,
-        text: `${from.name} revoked ${ViewType[viewKey].name} access from ${to.name}`, 
-    });
-}
-
-module.exports = { AccessRight, createAccessRight, deleteAccessRight };
+module.exports = { AccessRight };

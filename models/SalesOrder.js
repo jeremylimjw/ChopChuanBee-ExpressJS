20cef83e1d1cd96ef3747bdab6f0872575df3185
Jeremy <jeremylimjw@gmail.com>
2022-03-05 22:30:27 +0800

    major cleanup for so

--- models/SalesOrder.js
+++ models/SalesOrder.js
@@ -1,5 +1,12 @@
 const { DataTypes } = require('sequelize');
+const AccountingTypeEnum = require('../common/AccountingTypeEnum');
+const MovementType = require('../common/MovementTypeEnum');
+const PaymentTermType = require('../common/PaymentTermType');
 const { sequelize } = require('../db');
+const { Product } = require('./Product');
+const axios = require('axios');
+const PaymentMethodType = require('../common/PaymentMethodType');
+const { InventoryMovement } = require('./InventoryMovement');
 
 const SalesOrder = sequelize.define('sales_order', {
     id: {
@@ -45,6 +52,203 @@ const SalesOrderItem = sequelize.define('sales_order_item', {
     updatedAt: 'updated_at',
     createdAt: 'created_at',
 });
+
+
+
+// 'Save for later' use case
+async function updateSalesOrder(newSalesOrder) {
+    await SalesOrder.update(newSalesOrder, { where: { id: newSalesOrder.id } })
+
+    if (newSalesOrder.sales_order_items != null) {
+        await SalesOrderItem.destroy({ where: { sales_order_id: newSalesOrder.id }});
+        await SalesOrderItem.bulkCreate(newSalesOrder.sales_order_items);
+    }
+}
+
+
+// Validate sales order items and reduce duplicates
+function validateOrderItems(orderItems) {
+    // Reduce duplicated products
+    const reduced = [];
+    for (let item of orderItems) {
+        // Validation
+        if (!item.unit_price) throw 'Each order item must have a unit price.';
+        if (item.quantity <= 0) throw 'Each order item must have a valid quantity.';
+
+        const foundIndex = reduced.findIndex(x => x.product_id === item.product_id);
+        if (foundIndex >= 0) {
+            reduced[foundIndex].sum += item.quantity*item.unit_price;
+            reduced[foundIndex].count += item.quantity;
+        } else {
+            reduced.push({...item, sum: item.quantity*item.unit_price, count: item.quantity });
+        }
+    }
+
+    return reduced.map(x => ({...x, quantity: x.count, unit_price: x.sum/x.count }));
+}
+
+
+function buildNewPayment(salesOrderId, amount, paymentTermId, paymentMethodId) {
+    const payment = {
+        sales_order_id: salesOrderId, 
+        movement_type_id: MovementType.SALE.id,
+        amount: amount,
+    }
+
+    if (paymentTermId === PaymentTermType.CASH.id) {
+        payment.payment_method_id = paymentMethodId;
+    } else if (paymentTermId === PaymentTermType.CREDIT.id) {
+        payment.accounting_type_id = AccountingTypeEnum.RECEIVABLE.id;
+    }
+
+    return payment;
+}
+
+
+function buildRefundPayment(salesOrderId, amount, paymentTermId, paymentMethodId = PaymentMethodType.CASH.id) {
+    const payment = {
+        sales_order_id: salesOrderId, 
+        movement_type_id: MovementType.REFUND.id,
+        amount: -amount,
+    }
+
+    if (paymentTermId === PaymentTermType.CASH.id) {
+        payment.payment_method_id = paymentMethodId;
+    } else if (paymentTermId === PaymentTermType.CREDIT.id) {
+        payment.accounting_type_id = AccountingTypeEnum.RECEIVABLE.id;
+    }
+
+    return payment;
+}
+
+
+async function validateAndBuildNewInventories(salesOrder, orderItems) {
+    // Ensure validateOrderItems() is executed before this to make sure no duplicated product_ids
+    for (let item of orderItems) {
+      const stocks = await sequelize.query(
+        `
+        WITH o AS (SELECT COALESCE(-1*SUM(quantity),0) AS outflow FROM inventory_movements im WHERE quantity < 0 AND product_id = $1)
+        SELECT *, CASE WHEN (sum_over-(SELECT outflow FROM o)) > quantity THEN quantity ELSE (sum_over-(SELECT outflow FROM o)) END remaining
+          FROM  
+            (
+              SELECT 
+                created_at, product_Id, unit_cost, unit_price, quantity, movement_type_id, SUM(quantity) OVER(ORDER BY created_at) AS sum_over 
+              FROM inventory_movements im WHERE product_id = $1 AND quantity > 0
+            ) i  
+            WHERE i.sum_over-(SELECT outflow FROM o) >= 0 AND (sum_over-(SELECT outflow FROM o)) > 0;
+        `,
+        { 
+          bind: [item.product_id],
+          type: sequelize.QueryTypes.SELECT 
+        }
+      );
+      
+      const stockBalance = stocks.reduce((prev, current) => prev += +current.remaining, 0);
+  
+      if (stockBalance < item.quantity) {
+        const product = await Product.findByPk(item.product_id);
+        throw `${product.name} does not have enough stock (left ${stockBalance}) for order quantity ${item.quantity}`;
+      }
+  
+      let target = item.quantity;
+      let sum = 0;
+      let count = 0;
+      
+      for (let stock of stocks) {
+        let toAdd = target - count;
+        if (toAdd < stock.remaining) { // If got leftover
+          sum += toAdd * stock.unit_cost;
+          count += toAdd;
+        } else {
+          sum += stock.quantity * stock.unit_cost;
+          count += stock.quantity;
+        }
+        if (count >= target) break;
+      }
+  
+      item.unit_cost = sum/count;
+    }
+
+    const movements = orderItems.map(x => ({
+        sales_order_item_id: x.id,
+        product_id: x.product_id,
+        quantity: -x.quantity, // negative since its taking out from our inventory
+        unit_cost: x.unit_cost,
+        unit_price: x.unit_price*(1+salesOrder.gst_rate/100),
+        movement_type_id: MovementType.SALE.id,
+    }))
+      
+    return movements;
+}
+
+
+// { id: uuid (sales_order_item_id), top_up: number }
+// Calculates unit_cost for the movements
+async function buildRefundInventories(orderItems) {
+    // Calculate inventory movements to add
+    const inventoryMovements = [];
+    for (let item of orderItems) {
+        const salesOrderItem = await SalesOrderItem.findByPk(item.id);
+        const movements = await InventoryMovement.findAll({ where: { sales_order_item_id: item.id }})
+        let unitCost = 0;
+        for (let i = movements.length - 1; i >= 0; i--) { // iterate backwards
+          if (movements[i].quantity < 0) {
+            unitCost = movements[i].unit_cost; // if first inventory movement from the bottom of the array has negative quantity, it is the latest
+            break;
+          }
+        }
+  
+        const newMovement = {
+          product_id: salesOrderItem.product_id,
+          sales_order_item_id: salesOrderItem.id,
+          quantity: item.top_up, // should be positive sign because it is inventory incoming
+          unit_cost: unitCost,
+          movement_type_id: MovementType.REFUND.id,
+        }
+
+        inventoryMovements.push(newMovement);
+    }
+    
+    return inventoryMovements;
+}
+
+
+async function buildDeliveryOrder(salesOrder) {
+    // Retrieve geocoordinates of the delivery destination
+    const { data: geocode } = await axios.get(`https://developers.onemap.sg/commonapi/search`, {
+      params: {
+        searchVal: salesOrder.delivery_postal_code,     // Keywords entered by user that is used to filter out the results.
+        returnGeom: 'Y',            // Checks if user wants to return the geometry.
+        getAddrDetails: 'Y',        // Checks if user wants to return address details for a point.
+        pageNum: 1,                 // Specifies the page to retrieve your search results from.
+      }
+    })
+    
+    if (geocode.results.length === 0) {
+      throw `Could not find any location with the postal code ${salesOrder.delivery_postal_code}`;
+    }
+
+    const deliveryOrder = {
+        sales_order_id: salesOrder.id,
+        address: salesOrder.delivery_address,
+        postal_code: salesOrder.delivery_postal_code,
+        remarks: salesOrder.delivery_remarks,
+        longitude: geocode.results[0].LONGITUDE,
+        latitude: geocode.results[0].LATITUDE,
+    }
+  
+    return deliveryOrder;
+  }
   
 
-module.exports = { SalesOrder, SalesOrderItem };
+module.exports = { 
+    SalesOrder, 
+    SalesOrderItem, 
+    updateSalesOrder, 
+    validateAndBuildNewInventories, 
+    validateOrderItems,
+    buildNewPayment,
+    buildRefundPayment,
+    buildRefundInventories,
+    buildDeliveryOrder,
+};
--- routes/salesOrder.js
+++ routes/salesOrder.js
@@ -10,7 +10,7 @@ const { InventoryMovement } = require('../models/InventoryMovement');
 const { parseRequest, assertNotNull } = require('../common/helpers');
 const PurchaseOrderStatusType = require('../common/PurchaseOrderStatusType');
 const { ChargedUnder, Customer } = require('../models/Customer');
-const { SalesOrder, SalesOrderItem } = require('../models/SalesOrder');
+const { SalesOrder, SalesOrderItem, updateSalesOrder, buildNewPayment, buildRefundPayment, validateOrderItems, validateAndBuildNewInventories, buildDeliveryOrder, buildRefundInventories } = require('../models/SalesOrder');
 const { sequelize } = require('../db');
 const MovementType = require('../common/MovementTypeEnum');
 const PaymentTermType = require('../common/PaymentTermType');
@@ -25,8 +25,8 @@ router.get('/', requireAccess(ViewType.GENERAL), async function(req, res, next) 
 
   try {
     predicate.include = [
-      { model: SalesOrderItem, include: [InventoryMovement, Product] }, 
-      { model: Payment, include: [PaymentMethod] },
+      { model: SalesOrderItem, include: [InventoryMovement, Product] }, // im order by created_at desc
+      { model: Payment, include: [PaymentMethod] }, // payments by created_at desc
       Customer,
       ChargedUnder
     ];
@@ -84,46 +84,17 @@ router.put('/', requireAccess(ViewType.GENERAL), async function(req, res, next) 
   }
 
   try {
-    const result = await SalesOrder.update(req.body, { where: { id } });
+    await updateSalesOrder(req.body);
+    
+    // Record to admin logs
+    const user = res.locals.user;
+    await Log.create({ 
+      employee_id: user.id, 
+      view_id: ViewType.CRM.id,
+      text: `${user.name} edited sales order ID ${id}.`, 
+    });
 
-    if (result[0] === 0) {
-      res.status(400).send(`Sales order ID ${id} not found.`)
-
-    } else {
-
-      if (sales_order_items != null) {
-        // Update order items
-        const salesOrderItems = await SalesOrderItem.findAll({ where: { sales_order_id: id }});
-        // Remove deleted order items
-        for (let item of salesOrderItems) {
-          if (sales_order_items.filter(x => x.id == item.id).length === 0) {
-            await SalesOrderItem.destroy({ where: { id: item.id }});
-          }
-        }
-        // Upsert remaining items
-        for (let item of sales_order_items) {
-          await SalesOrderItem.upsert(item);
-        }
-      }
-
-      // Record to admin logs
-      const user = res.locals.user;
-      let text = `${user.name} updated sales order ID ${id}.`
-
-      if (sales_order_status_id != null) {
-        const opKey = Object.keys(PurchaseOrderStatusType).filter(key => PurchaseOrderStatusType[key].id == sales_order_status_id);
-        text = `${user.name} updated sales order ID ${id} to ${opKey.length ? PurchaseOrderStatusType[opKey].name : 'Unknown' }.`
-      }
-
-      await Log.create({ 
-        employee_id: user.id, 
-        view_id: ViewType.SCM.id,
-        text: text, 
-      });
-
-      res.send({ id: id });
-    }
-
+    res.send({ id: id });
 
   } catch(err) {
     // Catch and return any uncaught exceptions while inserting into database
@@ -149,70 +120,40 @@ router.post('/confirm', requireAccess(ViewType.GENERAL), async function(req, res
     const salesOrder = await SalesOrder.findByPk(id, { include: [{ model: SalesOrderItem, include: [Product] }] });
 
     // Calculate payment(s) to add
-    const payment = {
-      sales_order_id: salesOrder.id, 
-      movement_type_id: MovementType.SALE.id,
-    }
-
     let total = salesOrder.sales_order_items.reduce((prev, current) => prev + current.quantity * (current.unit_price || 0), 0) || 0;
     total = total * (1+salesOrder.gst_rate/100) + (+salesOrder.offset);
     total = Math.floor(total*100)/100; // Truncate trailing decimals 
 
-    if (salesOrder.payment_term_id === PaymentTermType.CASH.id) {
-      payment.amount = total;
-      payment.payment_method_id = salesOrder.payment_method_id;
+    const payment = buildNewPayment(salesOrder.id, total, salesOrder.payment_term_id, salesOrder.payment_method_id);
 
-    } else if (salesOrder.payment_term_id === PaymentTermType.CREDIT.id) {
-      payment.amount = -total;
-      payment.accounting_type_id = AccountingTypeEnum.RECEIVABLE.id;
-
-    }
-    
     // Calculate inventory movements to add
-    const inventoryMovements = salesOrder.sales_order_items.map(x => ({
-      product_id: x.product_id,
-      sales_order_item_id: x.id,
-      quantity: -x.quantity,
-      unit_price: x.unit_price*(1+salesOrder.gst_rate/100),
-      movement_type_id: MovementType.SALE.id,
-    }))
-    
-    // Validate Inventory Movements if have enough quantity
-    let reducedMovements = [];
+    let reducedOrderItems;
+    let inventoryMovements;
+    let deliveryOrder;
     try {
-      reducedMovements = await reduceInventoryMovements(inventoryMovements);
+      // Validate order items and reduce duplicate products
+      reducedOrderItems = validateOrderItems(salesOrder.toJSON().sales_order_items);
+      inventoryMovements = await validateAndBuildNewInventories(salesOrder, reducedOrderItems);
+      
+      if (salesOrder.has_delivery) {
+        deliveryOrder = await buildDeliveryOrder(salesOrder);
+      }
+
     } catch(err) {
       res.status(400).send(err);
       return;
     }
-    
-    // Add delivery order if needed
-    if (salesOrder.has_delivery) {
-      // Validate valid postal code location
-      let coords = {};
-      try {
-        coords = await getGeoCoords(salesOrder.delivery_postal_code)
-      } catch(err) {
-        res.status(400).send(err);
-        return;
-      }
 
-      const deliveryOrder = {
-        sales_order_id: salesOrder.id,
-        address: salesOrder.delivery_address,
-        postal_code: salesOrder.delivery_postal_code,
-        remarks: salesOrder.delivery_remarks,
-        ...coords,
-      }
-
+    // Update reduced order items
+    await updateSalesOrder({...salesOrder.toJSON(), sales_order_items: reducedOrderItems, sales_order_status_id: PurchaseOrderStatusType.ACCEPTED.id });
+    // Create payment
+    await Payment.create(payment);
+    // Create inventory Movements
+    await InventoryMovement.bulkCreate(inventoryMovements);
+    // Create delivery order
+    if (deliveryOrder) {
       await DeliveryOrder.create(deliveryOrder);
     }
-
-
-    await InventoryMovement.bulkCreate(reducedMovements);
-    await Payment.create(payment);
-    salesOrder.sales_order_status_id = PurchaseOrderStatusType.ACCEPTED.id;
-    salesOrder.save();
 
     // Record to admin logs
     const user = res.locals.user;
@@ -261,35 +202,21 @@ router.post('/cancel', requireAccess(ViewType.GENERAL), async function(req, res,
     });
 
     // Calculate payment(s) to add
-    const payment = {
-      sales_order_id: salesOrder.id,
-      movement_type_id: MovementType.REFUND.id,
-      amount: -(salesOrder.payments.filter(x => x.payment_method_id != null).reduce((prev, current) => prev += +current.amount, 0)), // flip to negative sign since its refund
-      accounting_type_id: salesOrder.payment_term_id === PaymentTermType.CREDIT.id ? 1 : null,
-      payment_method_id: PaymentMethodType.CASH.id,
-    }
+    const refundAmount = salesOrder.payments.filter(x => x.payment_method_id != null).reduce((prev, current) => prev += +current.amount, 0);
+    const payment = buildRefundPayment(salesOrder.id, refundAmount, salesOrder.payment_term_id);
 
-    // Calculate inventory movements to add
-    const inventoryMovements = salesOrder.sales_order_items.map(x => {
-      let unitCost = 0;
-      for (var i = x.inventory_movements.length - 1; i >= 0; i--) { // iterate backwards
-        if (x.inventory_movements[i].quantity < 0) {
-          unitCost = x.inventory_movements[i].unit_cost; // if first inventory movement from the bottom of the array has negative quantity, it is the latest
-          break;
-        }
-      }
+    // Calculate inventory movements to add, es unit_cost for the movements
+    const movements = salesOrder.toJSON().sales_order_items.map(x => ({
+      ...x, 
+      top_up: Math.abs(x.inventory_movements.reduce((prev, current) => prev + current.quantity, 0)), // sum will be in negative, Math.abs() to flip to positive
+    }))
+    const inventoryMovements = await buildRefundInventories(movements);
 
-      return {
-        product_id: x.product_id,
-        sales_order_item_id: x.id,
-        quantity: -(x.inventory_movements.reduce((prev, current) => prev + current.quantity, 0)), // flip negative sign to positive
-        unit_cost: unitCost,
-        movement_type_id: MovementType.REFUND.id,
-      }
-    })
-
-    await InventoryMovement.bulkCreate(inventoryMovements);
+    // Create payment
     await Payment.create(payment);
+    // Create inventory movements
+    await InventoryMovement.bulkCreate(inventoryMovements);
+    // Update sales order status
     salesOrder.sales_order_status_id = PurchaseOrderStatusType.CANCELLED.id;
     salesOrder.save();
 
@@ -319,19 +246,27 @@ router.post('/cancel', requireAccess(ViewType.GENERAL), async function(req, res,
 });
 
 
+// Handles new payment or refund payment
 router.post('/payment', requireAccess(ViewType.GENERAL), async function(req, res, next) {
-  const { sales_order_id, amount, payment_method_id, accounting_type_id, movement_type_id } = req.body;
+  const { sales_order_id, amount, type, payment_method_id } = req.body;
 
   // Validation here
   try {
-    assertNotNull(req.body, ['sales_order_id', 'amount', 'movement_type_id']);
+    assertNotNull(req.body, ['sales_order_id', 'amount', 'type']);
   } catch(err) {
     res.status(400).send(err);
     return;
   }
 
   try {
-    const newPayment = await Payment.create(req.body);
+    const salesOrder = await SalesOrder.findByPk(sales_order_id);
+    let payment;
+    if (type === 'NEW') {
+      payment = buildNewPayment(salesOrder.id, amount, salesOrder.payment_term_id, payment_method_id);
+    } else if (type === 'REFUND') {
+      payment = buildRefundPayment(salesOrder.id, amount, salesOrder.payment_term_id, payment_method_id);
+    }
+    const newPayment = await Payment.create(payment);
 
     // Record to admin logs
     const user = res.locals.user;
@@ -351,55 +286,6 @@ router.post('/payment', requireAccess(ViewType.GENERAL), async function(req, res
 });
 
 
-// For new sales order
-router.post('/inventory', requireAccess(ViewType.GENERAL), async function(req, res, next) {
-  const { inventory_movements } = req.body;
-
-  // Validation here
-  try {
-    assertNotNull(req.body, ['inventory_movements']);
-  } catch(err) {
-    res.status(400).send(err);
-    return;
-  }
-
-  // Validate Inventory Movements if have enough quantity
-  let reducedMovements = [];
-  try {
-    reducedMovements = await reduceInventoryMovements(inventory_movements);
-  } catch(err) {
-    res.status(400).send(err);
-    return;
-  }
-
-  try {
-    const newInventoryMovements = await InventoryMovement.bulkCreate(reducedMovements);
-
-    // Record to admin logs
-    const user = res.locals.user;
-    const logs = [];
-    for (let movement of inventory_movements) {
-      const salesOrderItem = await SalesOrderItem.findByPk(movement.sales_order_item_id, { include: Product });
-      logs.push(({ 
-        employee_id: user.id, 
-        view_id: ViewType.CRM.id,
-        text: `${user.name} made a sale for ${salesOrderItem.product.name} with ${Math.abs(movement.quantity)} quantity`, 
-      }))
-    }
-    await Log.bulkCreate(logs);
-
-    res.send(newInventoryMovements)
-
-  } catch(err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-
-});
-
-
-
-
 // For sales refund
 router.post('/inventory/refund', requireAccess(ViewType.GENERAL), async function(req, res, next) {
   const { inventory_movements } = req.body;
@@ -413,7 +299,8 @@ router.post('/inventory/refund', requireAccess(ViewType.GENERAL), async function
   }
 
   try {
-    const newInventoryMovements = await InventoryMovement.bulkCreate(inventory_movements);
+    const inventoryMovements = await buildRefundInventories(inventory_movements);
+    const newInventoryMovements = await InventoryMovement.bulkCreate(inventoryMovements);
 
     // Record to admin logs
     const user = res.locals.user;
@@ -436,90 +323,5 @@ router.post('/inventory/refund', requireAccess(ViewType.GENERAL), async function
   }
 
 });
-
-
-async function reduceInventoryMovements(inventory_movements) {
-  // Reduce duplicated product's quantity
-  const reducedMovements = [];
-  inventory_movements.forEach(movement => {
-    const index = reducedMovements.findIndex(x => x.product_id === movement.product_id);
-    if (index > -1) {
-      reducedMovements.quantity += +movement.quantity;
-    } else {
-      reducedMovements.push(movement)
-    }
-  });
-
-  for (let movement of reducedMovements) {
-    const stocks = await sequelize.query(
-      `
-      WITH o AS (SELECT COALESCE(-1*SUM(quantity),0) AS outflow FROM inventory_movements im WHERE quantity < 0 AND product_id = $1)
-      SELECT *, CASE WHEN (sum_over-(SELECT outflow FROM o)) > quantity THEN quantity ELSE (sum_over-(SELECT outflow FROM o)) END remaining
-        FROM  
-          (
-            SELECT 
-              created_at, product_Id, unit_cost, unit_price, quantity, movement_type_id, SUM(quantity) OVER(ORDER BY created_at) AS sum_over 
-            FROM inventory_movements im WHERE product_id = $1 AND quantity > 0
-          ) i  
-          WHERE i.sum_over-(SELECT outflow FROM o) >= 0 AND (sum_over-(SELECT outflow FROM o)) > 0;
-      `,
-      { 
-        bind: [movement.product_id],
-        type: sequelize.QueryTypes.SELECT 
-      }
-    );
-    
-    const stockBalance = stocks.reduce((prev, current) => prev += +current.remaining, 0);
-
-    if (stockBalance < Math.abs(movement.quantity)) {
-      const product = await Product.findByPk(movement.product_id);
-      throw `${product.name} does not have enough stock (left ${stockBalance}) for order quantity ${Math.abs(movement.quantity)}`;
-    }
-
-    let target = Math.abs(movement.quantity); // quantity in request will be negative
-    let sum = 0;
-    let count = 0;
-    
-    for (let stock of stocks) {
-      let toAdd = target - count;
-      if (toAdd < stock.remaining) { // If got leftover
-        sum += toAdd * stock.unit_cost;
-        count += toAdd;
-      } else {
-        sum += stock.quantity * stock.unit_cost;
-        count += stock.quantity;
-      }
-      if (count >= target) break;
-    }
-
-    movement.unit_cost = sum/count;
-  }
-    
-  return reducedMovements;
-}
-
-async function getGeoCoords(postal_code) {
-  // Retrieve geocoordinates of the delivery destination
-  const { data: geocode } = await axios.get(`https://developers.onemap.sg/commonapi/search`, {
-    params: {
-      searchVal: postal_code,     // Keywords entered by user that is used to filter out the results.
-      returnGeom: 'Y',            // Checks if user wants to return the geometry.
-      getAddrDetails: 'Y',        // Checks if user wants to return address details for a point.
-      pageNum: 1,                 // Specifies the page to retrieve your search results from.
-    }
-  })
-  
-  if (geocode.results.length === 0) {
-    throw `Could not find any location with the postal code ${postal_code}`;
-  }
-
-  const coords = {
-    longitude: geocode.results[0].LONGITUDE,
-    latitude: geocode.results[0].LATITUDE,
-  }
-
-  return coords;
-}
-
 
 module.exports = router;

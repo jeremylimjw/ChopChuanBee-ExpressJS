16c32f0a7f06cd0fa909ab17aaa660c2edfc2390
Enelton Satria <e0202920@u.nus.edu>
2022-03-12 16:49:21 +0800

    completed analytics: insights for each customer (revenue, cost, profit)

--- routes/analytics.js
+++ routes/analytics.js
@@ -10,6 +10,7 @@ const { parseRequest, assertNotNull } = require('../common/helpers');
 
 
 //1. Profits Dashboard: COGS
+// returns by selected month
 router.get('/COGS_table', requireAccess(ViewType.ANALYTICS, true), async function(req, res, next) {
     const {start_date ,end_date } = req.query;
     try {
@@ -45,6 +46,7 @@ router.get('/COGS_table', requireAccess(ViewType.ANALYTICS, true), async functio
 });
 
 //1. Profits Dashboard: Revenue
+// returns by selected month
 router.get('/Revenue_table', requireAccess(ViewType.ANALYTICS, true), async function(req, res, next) {
   const {start_date ,end_date } = req.query;
   try {
@@ -80,6 +82,7 @@ router.get('/Revenue_table', requireAccess(ViewType.ANALYTICS, true), async func
 });
 
 //1. Profits Dashboard: Profits
+// returns by selected months
 router.get('/Profits_table', requireAccess(ViewType.ANALYTICS, true), async function(req, res, next) {
   const {start_date ,end_date } = req.query;
   try {
@@ -128,7 +131,63 @@ router.get('/Profits_table', requireAccess(ViewType.ANALYTICS, true), async func
 });
 
 
-//2. Payments Dashboard: Unsettled AR -- customer level
+//2. Profits for each Customer
+// returns all the customers
+router.get('/Customer_Profits', requireAccess(ViewType.ANALYTICS, true), async function(req, res, next) {
+  const {start_date ,end_date } = req.query;
+  try {
+      const profitsTable = await sequelize.query(
+          `WITH invoiceProfits AS (
+            SELECT 
+                SUM(ims.rev) AS revenue, 
+                SUM(ims.cogs) AS cost_of_goods_sold, 
+                sales_order_item_id
+            FROM 
+                (
+                    SELECT (quantity*unit_price*-1) AS rev, 
+                    (quantity * unit_cost) AS cogs, 
+                    created_at, sales_order_item_id, 
+                    movement_type_id FROM inventory_movements
+                ) ims
+            WHERE movement_type_id = 2
+                AND created_at::DATE >= '${start_date}'
+                AND created_at::DATE <= '${end_date}'
+            GROUP BY sales_order_item_id
+            )
+            SELECT 
+                so.id AS sales_order_id, 
+                invoiceProfits.revenue AS total_revenue, 
+                invoiceProfits.cost_of_goods_sold*-1 AS total_COGS,
+                invoiceProfits.revenue +  invoiceProfits.cost_of_goods_sold AS total_profits
+            FROM invoiceProfits 
+            INNER JOIN sales_order_items poitems ON invoiceProfits.sales_order_item_id = poitems.id
+            INNER JOIN sales_orders so ON so.id = poitems.sales_order_id
+            INNER JOIN customers c ON so.customer_id = c.id; `,
+          {
+              raw: true,
+              type: sequelize.QueryTypes.SELECT
+          }
+      )
+
+      // Record to admin logs
+      const user = res.locals.user;
+      await Log.create({ 
+        employee_id: user.id, 
+        view_id: ViewType.ANALYTICS.id,
+        text: `${user.name} viewed the Profits for each Customer Dashboard`, 
+      });
+  
+      res.send(profitsTable);
+  
+    } catch(err) {
+      // Catch and return any uncaught exceptions while inserting into database
+      console.log(err);
+      res.status(500).send(err);
+    }
+});
+
+//3. Payments Dashboard: Unsettled AR -- customer level
+//returns top 10 customers
 router.get('/Customer_AR', requireAccess(ViewType.ANALYTICS, true), async function(req, res, next) {
    
   try {
@@ -167,7 +226,8 @@ router.get('/Customer_AR', requireAccess(ViewType.ANALYTICS, true), async functi
 });
 
 
-//2. Payments Dashboard: Unsettled AP -- supplier level
+//3. Payments Dashboard: Unsettled AP -- supplier level
+// returns top 10 supplier
 router.get('/Supplier_AP', requireAccess(ViewType.ANALYTICS, true), async function(req, res, next) {
    
   try {
@@ -206,7 +266,8 @@ router.get('/Supplier_AP', requireAccess(ViewType.ANALYTICS, true), async functi
 });
 
 
-//2. Payments Dashboard: Unsettled AP -- invoice level
+//3. Payments Dashboard: Unsettled AP -- invoice level
+// returns top 10 invoice
 router.get('/Unsettled_AP', requireAccess(ViewType.ANALYTICS, true), async function(req, res, next) {
    
     try {
@@ -241,7 +302,8 @@ router.get('/Unsettled_AP', requireAccess(ViewType.ANALYTICS, true), async funct
       }
 });
 
-//2. Payments Dashboard: Unsettled AR -- invoice level
+//3. Payments Dashboard: Unsettled AR -- invoice level
+// returns top 10 invoice
 router.get('/Unsettled_AR', requireAccess(ViewType.ANALYTICS, true), async function(req, res, next) {
    
   try {

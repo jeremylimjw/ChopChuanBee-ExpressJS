9aac759ab48872f52639fafa3b1d9c6075d151e6
Jeremy <jeremylimjw@gmail.com>
2022-03-23 02:35:35 +0800

    added models

---
+++ models/Chat.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../db');
+
+const Channel = sequelize.define('channel', {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    updatedAt: 'updated_at', // Standardize 'updatedAt' column name to 'updated_at'
+    createdAt: 'created_at', // Standardize 'createdAt' column name to 'created_at'
+});
+
+const Text = sequelize.define('text', {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
+    text: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    updatedAt: 'updated_at', // Standardize 'updatedAt' column name to 'updated_at'
+    createdAt: 'created_at', // Standardize 'createdAt' column name to 'created_at'
+});
+
+const Participant = sequelize.define('participant', {
+    last_received: DataTypes.DATE,
+    last_read: DataTypes.DATE,
+}, {
+    updatedAt: 'updated_at', // Standardize 'updatedAt' column name to 'updated_at'
+    createdAt: 'created_at', // Standardize 'createdAt' column name to 'created_at'
+});
+
+module.exports = { Channel, Text, Participant };

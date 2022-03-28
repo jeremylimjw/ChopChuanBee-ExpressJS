83db7cee0bf73c64f41dd4e46d27e0e4d860b4c7
NgJunKai <e0407444@u.nus.edu>
2022-03-28 12:50:00 +0800

    clean up accounting route

--- routes/accounting.js
+++ routes/accounting.js
@@ -10,7 +10,7 @@ const { Sequelize } = require('sequelize');
 const { sequelize } = require('../db');
 const { parseRequest, assertNotNull } = require('../common/helpers');
 
-//sofp statement of financial position 
+//sofp get
 router.get('/SOFP', requireAccess(ViewType.ACCOUNTING, false), async function(req, res, next) {
     const predicate = parseRequest(req.query);
   
@@ -25,6 +25,217 @@ router.get('/SOFP', requireAccess(ViewType.ACCOUNTING, false), async function(re
   }
 
 });
+
+//SOFP post
+router.post('/sofp', requireAccess(ViewType.ACCOUNTING, true), async function(req, res, next) {
+  const {name,end_date , remarks } = req.body;
+  try {
+
+      const account_receivable = await sequelize.query(
+          `SELECT sum(amount) as AR
+          FROM payments
+          WHERE accounting_type_id = 1 
+          AND created_at <= '${end_date}'`,
+          { 
+              raw: true, 
+              type: sequelize.QueryTypes.SELECT 
+          }
+      );
+      const account_payable = await sequelize.query(
+          `SELECT sum(amount) as AP
+          FROM payments
+          WHERE accounting_type_id = 2 
+          AND created_at <= '${end_date}'`,
+          { 
+            raw: true, 
+            type: sequelize.QueryTypes.SELECT 
+          }
+        );
+  
+        const cash_sales_of_goods = await sequelize.query(
+          `SELECT sum(amount) as cash_sales_of_goods
+          FROM payments
+          WHERE payment_method_id IS NOT NULL  
+          AND created_at <= '${end_date}'
+          `,
+          { 
+            raw: true, 
+            type: sequelize.QueryTypes.SELECT 
+          }
+        );
+  
+  
+        const inventory  = await sequelize.query(
+          `SELECT sum(qty_unitcost.total) as inv_at_hand
+          FROM (select (quantity * unit_cost) as total, created_at FROM inventory_movements) qty_unitcost
+          WHERE created_at <= '${end_date}'
+          `,
+          { 
+            raw: true, 
+            type: sequelize.QueryTypes.SELECT 
+          }
+        );
+       
+        const cash_business = parseFloat(cash_sales_of_goods[0].cash_sales_of_goods) || 0;
+        const ar = parseFloat(account_receivable[0].ar) || 0;
+        const ap = parseFloat(account_payable[0].ap) || 0;
+        const inv_at_hand = parseFloat(inventory[0].inv_at_hand) || 0;
+        const newSOFP = await SOFP.create({name,cash_sales_of_goods:cash_business, account_receivable:ar, inventory:inv_at_hand,account_payable:ap, end_date ,remarks});
+    
+      // Record to admin logs
+      const user = res.locals.user;
+      await Log.create({ 
+        employee_id: user.id, 
+        view_id: ViewType.ACCOUNTING.id,
+        text: `${user.name} created a SOFP record `, 
+      });
+  
+      res.send(newSOFP.toJSON());
+  
+    } catch(err) {
+      // Catch and return any uncaught exceptions while inserting into database
+      console.log(err);
+      res.status(500).send(err);
+    }
+
+
+});
+
+//SOFP PUT
+router.put('/SOFP', requireAccess(ViewType.ACCOUNTING, true), async function(req, res, next) {
+  const { id, name,cash_sales_of_goods, cash_others, account_receivable, inventory, supplies, prepaid_insurance, prepaid_rent, other_current_asset_1,other_current_asset_2, land, less_accumulated_depreciation_land, building, less_accumulated_depreciation_building, equipments, less_accumulated_depreciation_equipments, other_non_current_asset_1, other_non_current_asset_2, goodwill, trade_names, other_intangible_asset_1, other_intangible_asset_2, account_payable, salary_payable, interest_payable, taxes_payable, warranty_payable, rental_payable, notes_payable, bonds_payable, other_current_liability_1, other_current_liability_2, other_non_current_liability_1, other_non_current_liability_2, share_capital, less_withdrawal, retained_earning, other_equity_1, other_equity_2,end_date , remarks} = req.body;
+
+
+  try {
+    assertNotNull(req.body, ['id', 'name', 'cash_sales_of_goods', 'cash_others', 
+                              'account_receivable', 'inventory', 'supplies', 'prepaid_insurance', 
+                              'prepaid_rent', 'other_current_asset_1', 'other_current_asset_2', 
+                              'land', 'less_accumulated_depreciation_land', 'building', 
+                              'less_accumulated_depreciation_building', 'equipments', 
+                              'less_accumulated_depreciation_equipments', 'other_non_current_asset_1', 
+                              'other_non_current_asset_2', 'goodwill', 'trade_names', 
+                              'other_intangible_asset_1', 'other_intangible_asset_2', 'account_payable', 'salary_payable', 
+                              'interest_payable', 'taxes_payable', 'warranty_payable', 'rental_payable', 
+                              'notes_payable', 'bonds_payable', 'other_current_liability_1', 'other_current_liability_2','other_non_current_liability_1','other_non_current_liability_2',
+                              'share_capital', 'less_withdrawal', 'retained_earning', 'other_equity_1', 
+                              'other_equity_2', 'end_date'])
+  } catch(err) {
+    res.status(400).send(err);
+    return;
+  }
+
+
+  try {
+    const result = await SOFP.update(
+      { name, cash_sales_of_goods, cash_others, account_receivable, inventory, supplies, prepaid_insurance, prepaid_rent, other_current_asset_1,other_current_asset_2, land, less_accumulated_depreciation_land, building, less_accumulated_depreciation_building, equipments, less_accumulated_depreciation_equipments, other_non_current_asset_1, other_non_current_asset_2, goodwill, trade_names, other_intangible_asset_1, other_intangible_asset_2,account_payable, salary_payable, interest_payable, taxes_payable, warranty_payable, rental_payable, notes_payable, bonds_payable, other_current_liability_1, other_current_liability_2,other_non_current_liability_1, other_non_current_liability_2, share_capital, less_withdrawal, retained_earning, other_equity_1, other_equity_2, end_date , remarks },
+      { where: { id : id } }
+    );
+
+    // If 'id' is not found return 400 Bad Request, if found then return the 'id'
+    if (result[0] === 0) {
+      res.status(400).send(`SOFP id ${id} not found.`)
+
+    } else {
+      // Record to admin logs
+      const user = res.locals.user;
+      await Log.create({ 
+        employee_id: user.id, 
+        view_id: ViewType.ACCOUNTING.id,
+        text: `${user.name} updated ${name} SOFP`, 
+      });
+
+      res.send({ id: id });
+    }
+
+
+  } catch(err) {
+    // Catch and return any uncaught exceptions while inserting into database
+    console.log(err);
+    res.status(500).send(err);
+  }
+
+});
+
+//deactivate sofp 
+router.post('/SOFP/deactivate', requireAccess(ViewType.ACCOUNTING, true), async function(req, res, next) {
+  const { id } = req.body;
+
+  // Attribute validation here. You can go as deep as type validation but this here is the minimal validation
+  if (id == null) {
+    res.status(400).send("'id' is required.", )
+    return;
+  }
+
+  try {
+    const sofp = await SOFP.findByPk(id);
+
+    // If 'id' is not found return 400 Bad Request, if found then return the 'id'
+    if (sofp == null) {
+      res.status(400).send(`sofp id ${id} not found.`)
+
+    } else {
+      sofp.deleted_date = new Date();
+      sofp.save();
+
+      // Record to admin logs
+      const user = res.locals.user;
+      await Log.create({ 
+        employee_id: user.id, 
+        view_id: ViewType.ACCOUNTING.id,
+        text: `${user.name} deleted ${sofp.name}`, 
+      });
+
+      res.send({ id: sofp.id, deleted_date: sofp.deleted_date});
+    }
+
+
+  } catch(err) {
+    // Catch and return any uncaught exceptions while inserting into database
+    console.log(err);
+    res.status(500).send(err);
+  }
+
+});
+//activate SOFP
+router.post('/SOFP/activate', requireAccess(ViewType.ACCOUNTING, true), async function(req, res, next) {
+  const { id } = req.body;
+
+  if (id == null) {
+    res.status(400).send("'id' is required.", )
+    return;
+  }
+
+  try {
+    const sofp = await SOFP.findByPk(id);
+
+    if (sofp == null) {
+      res.status(400).send(`sofp id ${id} not found.`)
+
+    } else {
+      sofp.deleted_date = null;
+      sofp.save();
+
+      // Record to admin logs
+      const user = res.locals.user;
+      await Log.create({ 
+        employee_id: user.id, 
+        view_id: ViewType.CRM.id,
+        text: `$${user.name} reactivated  ${sofp.name}`, 
+      });
+
+      res.send({ id: sofp.id, deleted_date: null });
+    }
+
+
+  } catch(err) {
+    // Catch and return any uncaught exceptions while inserting into database
+    console.log(err);
+    res.status(500).send(err);
+  }
+
+});
+
+
 
 //incomeStatement  
 router.get('/income_statement', requireAccess(ViewType.ACCOUNTING, false), async function(req, res, next) {
@@ -77,80 +288,7 @@ router.get('/income_statement', requireAccess(ViewType.ACCOUNTING, false), async
     }
 });
 
-//SOFP
-router.post('/sofp', requireAccess(ViewType.ACCOUNTING, true), async function(req, res, next) {
-    const {name,end_date , remarks } = req.body;
-    try {
 
-        const account_receivable = await sequelize.query(
-            `SELECT sum(amount) as AR
-            FROM payments
-            WHERE accounting_type_id = 1 
-            AND created_at <= '${end_date}'`,
-            { 
-                raw: true, 
-                type: sequelize.QueryTypes.SELECT 
-            }
-        );
-        const account_payable = await sequelize.query(
-            `SELECT sum(amount) as AP
-            FROM payments
-            WHERE accounting_type_id = 2 
-            AND created_at <= '${end_date}'`,
-            { 
-              raw: true, 
-              type: sequelize.QueryTypes.SELECT 
-            }
-          );
-    
-          const cash_sales_of_goods = await sequelize.query(
-            `SELECT sum(amount) as cash_sales_of_goods
-            FROM payments
-            WHERE payment_method_id IS NOT NULL  
-            AND created_at <= '${end_date}'
-            `,
-            { 
-              raw: true, 
-              type: sequelize.QueryTypes.SELECT 
-            }
-          );
-    
-    
-          const inventory  = await sequelize.query(
-            `SELECT sum(qty_unitcost.total) as inv_at_hand
-            FROM (select (quantity * unit_cost) as total, created_at FROM inventory_movements) qty_unitcost
-            WHERE created_at <= '${end_date}'
-            `,
-            { 
-              raw: true, 
-              type: sequelize.QueryTypes.SELECT 
-            }
-          );
-         
-          const cash_business = parseFloat(cash_sales_of_goods[0].cash_sales_of_goods) || 0;
-          const ar = parseFloat(account_receivable[0].ar) || 0;
-          const ap = parseFloat(account_payable[0].ap) || 0;
-          const inv_at_hand = parseFloat(inventory[0].inv_at_hand) || 0;
-          const newSOFP = await SOFP.create({name,cash_sales_of_goods:cash_business, account_receivable:ar, inventory:inv_at_hand,account_payable:ap, end_date ,remarks});
-      
-        // Record to admin logs
-        const user = res.locals.user;
-        await Log.create({ 
-          employee_id: user.id, 
-          view_id: ViewType.ACCOUNTING.id,
-          text: `${user.name} created a SOFP record `, 
-        });
-    
-        res.send(newSOFP.toJSON());
-    
-      } catch(err) {
-        // Catch and return any uncaught exceptions while inserting into database
-        console.log(err);
-        res.status(500).send(err);
-      }
-
-
-});
 
 //create income statement 
 router.post('/income_statement', requireAccess(ViewType.ACCOUNTING, true), async function(req, res, next) {
@@ -215,61 +353,6 @@ router.post('/income_statement', requireAccess(ViewType.ACCOUNTING, true), async
 
 
 });
-//SOFP PUT
-router.put('/SOFP', requireAccess(ViewType.ACCOUNTING, true), async function(req, res, next) {
-    const { id, name,cash_sales_of_goods, cash_others, account_receivable, inventory, supplies, prepaid_insurance, prepaid_rent, other_current_asset_1,other_current_asset_2, land, less_accumulated_depreciation_land, building, less_accumulated_depreciation_building, equipments, less_accumulated_depreciation_equipments, other_non_current_asset_1, other_non_current_asset_2, goodwill, trade_names, other_intangible_asset_1, other_intangible_asset_2, account_payable, salary_payable, interest_payable, taxes_payable, warranty_payable, rental_payable, notes_payable, bonds_payable, other_current_liability_1, other_current_liability_2, other_non_current_liability_1, other_non_current_liability_2, share_capital, less_withdrawal, retained_earning, other_equity_1, other_equity_2,end_date , remarks} = req.body;
-  
-
-    try {
-      assertNotNull(req.body, ['id', 'name', 'cash_sales_of_goods', 'cash_others', 
-                                'account_receivable', 'inventory', 'supplies', 'prepaid_insurance', 
-                                'prepaid_rent', 'other_current_asset_1', 'other_current_asset_2', 
-                                'land', 'less_accumulated_depreciation_land', 'building', 
-                                'less_accumulated_depreciation_building', 'equipments', 
-                                'less_accumulated_depreciation_equipments', 'other_non_current_asset_1', 
-                                'other_non_current_asset_2', 'goodwill', 'trade_names', 
-                                'other_intangible_asset_1', 'other_intangible_asset_2', 'account_payable', 'salary_payable', 
-                                'interest_payable', 'taxes_payable', 'warranty_payable', 'rental_payable', 
-                                'notes_payable', 'bonds_payable', 'other_current_liability_1', 'other_current_liability_2','other_non_current_liability_1','other_non_current_liability_2',
-                                'share_capital', 'less_withdrawal', 'retained_earning', 'other_equity_1', 
-                                'other_equity_2', 'end_date'])
-    } catch(err) {
-      res.status(400).send(err);
-      return;
-    }
-  
-  
-    try {
-      const result = await SOFP.update(
-        { name, cash_sales_of_goods, cash_others, account_receivable, inventory, supplies, prepaid_insurance, prepaid_rent, other_current_asset_1,other_current_asset_2, land, less_accumulated_depreciation_land, building, less_accumulated_depreciation_building, equipments, less_accumulated_depreciation_equipments, other_non_current_asset_1, other_non_current_asset_2, goodwill, trade_names, other_intangible_asset_1, other_intangible_asset_2,account_payable, salary_payable, interest_payable, taxes_payable, warranty_payable, rental_payable, notes_payable, bonds_payable, other_current_liability_1, other_current_liability_2,other_non_current_liability_1, other_non_current_liability_2, share_capital, less_withdrawal, retained_earning, other_equity_1, other_equity_2, end_date , remarks },
-        { where: { id : id } }
-      );
-  
-      // If 'id' is not found return 400 Bad Request, if found then return the 'id'
-      if (result[0] === 0) {
-        res.status(400).send(`SOFP id ${id} not found.`)
-  
-      } else {
-        // Record to admin logs
-        const user = res.locals.user;
-        await Log.create({ 
-          employee_id: user.id, 
-          view_id: ViewType.ACCOUNTING.id,
-          text: `${user.name} updated ${name} SOFP`, 
-        });
-  
-        res.send({ id: id });
-      }
-  
-  
-    } catch(err) {
-      // Catch and return any uncaught exceptions while inserting into database
-      console.log(err);
-      res.status(500).send(err);
-    }
-  
-  });
-  
 
 //income statement PUT
 router.put('/income_statement', requireAccess(ViewType.ACCOUNTING, true), async function(req, res, next) {
@@ -317,85 +400,6 @@ router.put('/income_statement', requireAccess(ViewType.ACCOUNTING, true), async 
     }
   
   });
-//deactivate sofp 
-router.post('/SOFP/deactivate', requireAccess(ViewType.ACCOUNTING, true), async function(req, res, next) {
-    const { id } = req.body;
-  
-    // Attribute validation here. You can go as deep as type validation but this here is the minimal validation
-    if (id == null) {
-      res.status(400).send("'id' is required.", )
-      return;
-    }
-  
-    try {
-      const sofp = await SOFP.findByPk(id);
-  
-      // If 'id' is not found return 400 Bad Request, if found then return the 'id'
-      if (sofp == null) {
-        res.status(400).send(`sofp id ${id} not found.`)
-  
-      } else {
-        sofp.deleted_date = new Date();
-        sofp.save();
-  
-        // Record to admin logs
-        const user = res.locals.user;
-        await Log.create({ 
-          employee_id: user.id, 
-          view_id: ViewType.ACCOUNTING.id,
-          text: `${user.name} deleted ${sofp.name}`, 
-        });
-  
-        res.send({ id: sofp.id, deleted_date: sofp.deleted_date});
-      }
-  
-  
-    } catch(err) {
-      // Catch and return any uncaught exceptions while inserting into database
-      console.log(err);
-      res.status(500).send(err);
-    }
-  
-  });
-  //activate SOFP
-  router.post('/SOFP/activate', requireAccess(ViewType.ACCOUNTING, true), async function(req, res, next) {
-    const { id } = req.body;
-  
-    if (id == null) {
-      res.status(400).send("'id' is required.", )
-      return;
-    }
-  
-    try {
-      const sofp = await SOFP.findByPk(id);
-  
-      if (sofp == null) {
-        res.status(400).send(`sofp id ${id} not found.`)
-  
-      } else {
-        sofp.deleted_date = null;
-        sofp.save();
-  
-        // Record to admin logs
-        const user = res.locals.user;
-        await Log.create({ 
-          employee_id: user.id, 
-          view_id: ViewType.CRM.id,
-          text: `$${user.name} reactivated  ${sofp.name}`, 
-        });
-  
-        res.send({ id: sofp.id, deleted_date: null });
-      }
-  
-  
-    } catch(err) {
-      // Catch and return any uncaught exceptions while inserting into database
-      console.log(err);
-      res.status(500).send(err);
-    }
-  
-  });
-
 
 
 //deactivate income statement 
@@ -573,11 +577,6 @@ router.get('/output_tax', requireAccess(ViewType.ACCOUNTING, false), async funct
   }
 
 });
-
-
-
-
-
 
 
 

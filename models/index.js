9b957987455a6ba5cb027c2034cbfe5fd3041c16
Enelton Satria <e0202920@u.nus.edu>
2022-03-29 09:39:30 +0800

    added COGS small cards: previous month, current month, today

--- models/index.js
+++ models/index.js
@@ -213,8 +213,8 @@ async function syncAssociations() {
     const SOFP = require('../models/SOFP');
     const IncomeStatement = require('../models/IncomeStatement');
     
-    await sequelize.sync(); // This will create tables if not exists
-    // await sequelize.sync({ force: true }); // ONLY USE THIS FOR TESTING. This will ALWAYS drop tables and then create
+    // await sequelize.sync(); // This will create tables if not exists
+     await sequelize.sync({ force: true }); // ONLY USE THIS FOR TESTING. This will ALWAYS drop tables and then create
     // await sequelize.sync({ alter: true }); // ONLY USE THIS FOR TESTING. This checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
     
 }
--- routes/analytics.js
+++ routes/analytics.js
@@ -15,7 +15,7 @@ router.get('/COGS_table', requireAccess(ViewType.ANALYTICS, true), async functio
     const {start_date ,end_date } = req.query;
     try {
         const cogsTable = await sequelize.query(
-            `SELECT to_char(created_at, 'YYYY-MM') AS date, SUM(qty_unitcost.total) AS value
+            `SELECT to_char(created_at, 'YYYY-MM') AS date, SUM(qty_unitcost.total*-1) AS value
             FROM (SELECT (quantity * unit_cost) AS total, created_at, movement_type_id FROM inventory_movements) qty_unitcost
             WHERE movement_type_id = 2
             AND created_at::DATE >= '${start_date}'
@@ -34,6 +34,117 @@ router.get('/COGS_table', requireAccess(ViewType.ANALYTICS, true), async functio
           employee_id: user.id, 
           view_id: ViewType.ANALYTICS.id,
           text: `${user.name} viewed the Profits Dashboard (COGS) `, 
+        });
+    
+        res.send(cogsTable);
+    
+      } catch(err) {
+        // Catch and return any uncaught exceptions while inserting into database
+        console.log(err);
+        res.status(500).send(err);
+      }
+});
+
+//1. Profits Dashboard - Small Card for COGS
+// returns by selected month
+router.get('/COGS_table_CurrentMonth', requireAccess(ViewType.ANALYTICS, true), async function(req, res, next) {
+
+    try {
+        const cogsTable = await sequelize.query(
+            `SELECT to_char(created_at, 'YYYY-MM') AS date, SUM(qty_unitcost.total*-1) AS value
+              FROM (SELECT (quantity * unit_cost) AS total, created_at, movement_type_id
+                  FROM inventory_movements) qty_unitcost
+              WHERE movement_type_id = 2
+              GROUP BY date
+              ORDER BY date DESC
+              LIMIT 1;`,
+            {
+                raw: true,
+                type: sequelize.QueryTypes.SELECT
+            }
+        )
+
+        // Record to admin logs
+        const user = res.locals.user;
+        await Log.create({ 
+          employee_id: user.id, 
+          view_id: ViewType.ANALYTICS.id,
+          text: `${user.name} viewed the Profits Dashboard (COGS_Current_Month) `, 
+        });
+    
+        res.send(cogsTable);
+    
+      } catch(err) {
+        // Catch and return any uncaught exceptions while inserting into database
+        console.log(err);
+        res.status(500).send(err);
+      }
+});
+
+//1. Profits Dashboard - Small Card for COGS
+// returns by selected month
+router.get('/COGS_table_previousmonth', requireAccess(ViewType.ANALYTICS, true), async function(req, res, next) {
+
+    try {
+        const cogsTable = await sequelize.query(
+            `
+            SELECT to_char(created_at, 'YYYY-MM') AS date, SUM(qty_unitcost.total*-1) AS value
+            FROM (SELECT (quantity * unit_cost) AS total, created_at, movement_type_id
+                FROM inventory_movements) qty_unitcost
+            WHERE movement_type_id = 2
+            GROUP BY date
+            ORDER BY date DESC
+            LIMIT 1 OFFSET 1;
+            `,
+            {
+                raw: true,
+                type: sequelize.QueryTypes.SELECT
+            }
+        )
+
+        // Record to admin logs
+        const user = res.locals.user;
+        await Log.create({ 
+          employee_id: user.id, 
+          view_id: ViewType.ANALYTICS.id,
+          text: `${user.name} viewed the Profits Dashboard (COGS_Previous_Month) `, 
+        });
+    
+        res.send(cogsTable);
+    
+      } catch(err) {
+        // Catch and return any uncaught exceptions while inserting into database
+        console.log(err);
+        res.status(500).send(err);
+      }
+});
+
+//1. Profits Dashboard - Small Card for COGS
+// returns by selected month
+router.get('/COGS_table_today', requireAccess(ViewType.ANALYTICS, true), async function(req, res, next) {
+
+    try {
+        const cogsTable = await sequelize.query(
+            `
+            SELECT created_at AS date, SUM(qty_unitcost.total*-1) AS value
+            FROM (SELECT (quantity * unit_cost) AS total, created_at, movement_type_id
+                FROM inventory_movements) qty_unitcost
+            WHERE movement_type_id = 2
+                AND created_at = CURRENT_DATE
+            GROUP BY created_at;
+            `,
+            {
+                raw: true,
+                type: sequelize.QueryTypes.SELECT
+            }
+        )
+
+        // Record to admin logs
+        const user = res.locals.user;
+        await Log.create({ 
+          employee_id: user.id, 
+          view_id: ViewType.ANALYTICS.id,
+          text: `${user.name} viewed the Profits Dashboard (COGS_Today) `, 
         });
     
         res.send(cogsTable);
